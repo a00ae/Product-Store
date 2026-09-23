01273118ccf6c9fb69948dcadafa60812d7e9a08
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-09-23 18:46:48 +0300

    Adding a search function

--- main.js
+++ main.js
@@ -46,8 +46,7 @@ submit.onclick = () => {
   };
 
   if (mood === "create") {
-
-    if (newTach.count < 1 ) {
+    if (newTach.count < 1) {
       count.style.border = "1px solid #f04";
       return;
     }
@@ -148,9 +147,78 @@ const updateData = (id) => {
 
   scroll({
     top: 0,
-    behavior: "smooth"
-  })
+    behavior: "smooth",
+  });
 };
 // search
-// clean data
 
+// search  mood
+
+let searchMood = "title";
+
+const getSearchMood = (id) => {
+  let search = document.getElementById("search");
+  if (id === "search-by-title") {
+    searchMood = "title";
+  } else {
+    searchMood = "catagory";
+  }
+
+  search.placeholder = `search by ${searchMood}` 
+  search.value = '';
+  search.focus();
+  readData();
+  console.log(searchMood);
+};
+
+const searchData = (value) => {
+  let table = "";
+
+  for (let i = 0; i < dataTuch.length; i++) {
+    if (searchMood === "title") {
+      if (
+        dataTuch[i].title.toLowerCase().includes(value.toLowerCase().trim())
+      ) {
+        table += `
+                    <tr>
+                      <td>${i + 1}</td>
+                      <td>${dataTuch[i].title}</td>
+                      <td>${dataTuch[i].price}</td>
+                      <td>${dataTuch[i].taxes}</td>
+                      <td>${dataTuch[i].ads}</td>
+                      <td>${dataTuch[i].discount}</td>
+                      <td>${dataTuch[i].count}</td>
+                      <td>${dataTuch[i].total}</td>
+                      <td>${dataTuch[i].catagory}</td>
+                      <td><button onclick="updateData(${i})" id="update">update</button></td>
+                      <td><button onclick="deleteData(${i})" id="delete">delete</button></td>
+                    </tr>
+                   `;
+      }
+    } else {
+      if (
+        dataTuch[i].catagory.toLowerCase().includes(value.toLowerCase().trim())
+      ) {
+        table += `
+                    <tr>
+                      <td>${i + 1}</td>
+                      <td>${dataTuch[i].title}</td>
+                      <td>${dataTuch[i].price}</td>
+                      <td>${dataTuch[i].taxes}</td>
+                      <td>${dataTuch[i].ads}</td>
+                      <td>${dataTuch[i].discount}</td>
+                      <td>${dataTuch[i].count}</td>
+                      <td>${dataTuch[i].total}</td>
+                      <td>${dataTuch[i].catagory}</td>
+                      <td><button onclick="updateData(${i})" id="update">update</button></td>
+                      <td><button onclick="deleteData(${i})" id="delete">delete</button></td>
+                    </tr>
+                    `;
+      }
+    }
+  }
+
+  document.getElementById("tbody").innerHTML = table;
+};
+
+// clean data
